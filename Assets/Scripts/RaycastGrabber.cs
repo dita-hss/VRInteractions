c56e6f61ecb87bcb331edcf0633e6cfc0e46f163
dita-hss <101124244+dita-hss@users.noreply.github.com>
2024-10-25 14:33:31 -0600

    10/25 updates

--- Assets/Scripts/RaycastGrabber.cs
+++ Assets/Scripts/RaycastGrabber.cs
@@ -12,7 +12,12 @@ public class RaycastGrabber : MonoBehaviour
     [SerializeField]
     private InputActionReference teleportButtonPress;
     [SerializeField]   
-    private Transform parentTransform;
+    private Transform rightHandTransform;
+    [SerializeField]   
+    private Transform leftHandTransform;
+
+    // hoolding weapon?
+    private bool holdingWeapon = false;
 
     void Start()
     {
@@ -28,21 +33,42 @@ public class RaycastGrabber : MonoBehaviour
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
         // Parameters: position to start the ray, direction to project the ray, output for raycast, limit of ray length, and layer mask
-        Debug.Log(Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, teleportMask));
+        //Debug.Log(Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, teleportMask));
         if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, teleportMask)) {
 
             // The object we hit will be in the collider property of our hit object.
             // We can get the name of that object by accessing it's Game Object then the name property
-            Debug.Log("here" + hit.collider.gameObject.name);
             
             // Don't forget to attach the player origin in the editor!
             //playerOrigin.transform.position = hit.collider.gameObject.transform.position;
-            GameObject newObject = Instantiate(hit.collider.gameObject, hit.point, Quaternion.identity, parentTransform);
+
+            
+
+
+            
+            // if currently holding weapon, destroy the weapon being held, and replace it with the new weapon
+            if (holdingWeapon) {
+                // destroy the weapon
+                Destroy(rightHandTransform.GetChild(0).gameObject);
+                // current weapon is not being destroyed
+                Debug.Log("hit object: " + rightHandTransform.GetChild(0).gameObject);
+            }
+            
+            GameObject newObject = Instantiate(hit.collider.gameObject, rightHandTransform.position, rightHandTransform.rotation, rightHandTransform);
             Debug.Log("object " + newObject + " " + newObject.name);
-            //newObject.transform.SetParent(parentTransform, false);
+            newObject.transform.SetParent(rightHandTransform, false);
+            holdingWeapon = true;
+
+
+            //newObject.transform.localPosition = Vector3.zero;
+            // rotate 45 degrees
+            newObject.transform.localRotation = Quaternion.Euler(0, -90, 0);
+
 
         }
 
     }
 }
+
+
 
